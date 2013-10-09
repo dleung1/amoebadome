2293a74a546ad4576451af7c86b3cfa0c25a61ae
Dave Kushner <dkushner2@mail.csuchico.edu>
2013-10-09 14:01:20 -0700

    Update client to test network replication.

--- js/components/network.js
+++ js/components/network.js
@@ -14,14 +14,18 @@ Network.prototype.constructor = Network;
 Network.prototype.setOwner = function(owner) {
   var data = _.reduce(this._targets, function(ag, el) {
     ag[el] = _.deep(owner, el);
+    return ag;
   }, {});
   this._socket.emit('agentNew', data);
   Component.prototype.setOwner.call(this, owner);
 };
 
 Network.prototype.update = function(dt) {
-  var data = _.reduce(this._targets, function(ag, el) {
-    ag[el] = _.deep(this._owner, el);
-  }, {});
-  this._socket.emit('agentTick', data);
+  if (this._owner) {
+    var data = _.reduce(this._targets, function(ag, el) {
+      ag[el] = _.deep(this._owner, el)
+      return ag;
+    }, {}, this);
+    this._socket.emit('agentTick', data);
+  }
 }
